bf59904d32d9179ea0e99d8a1af7bd04717bc0d9
Srinivas Mamidi <srinivasmamidi996@gmail.com>
2024-07-31 17:19:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-x');
+    navbar.classList.toggle('active')
+}
+
+
+
+let section = document.querySelector('section');
+let sections = document.querySelector('head nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEaach.apply(links => {
+                links.classlist.remove('active');
+                document.querySelector('header nav a[href*='+ id + ']');
+            });
+        }
+    });
+
+
+    let  header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
